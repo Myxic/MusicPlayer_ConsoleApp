40a673fa8a98b27a01e297c21122a3ea7f147a1e
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-12-01 22:55:50 +0100

    upload

--- MusicPlayer_ConsoleApp/EditMusicApp.cs
+++ MusicPlayer_ConsoleApp/EditMusicApp.cs
@@ -8,7 +8,6 @@ namespace MusicPlayer_ConsoleApp
 {
     public class EditMusicApp : PlaylistOperation
     {
-        //private static int Count = 0;
         public EditMusicApp()
         {
         }
@@ -152,14 +151,14 @@ namespace MusicPlayer_ConsoleApp
                             DeleteSong(Index);
                             break;
 
-                          
+
                     }
                 }
                 else
                 {
                     Console.Clear();
                     Console.WriteLine($"{Index} is an invalid Option");
-            
+
                     goto start;
                 }
             }
@@ -167,7 +166,7 @@ namespace MusicPlayer_ConsoleApp
             {
                 Console.Clear();
                 Console.WriteLine($"{Option} is an invalid Option");
-                
+
                 goto start;
             }
         }
@@ -177,7 +176,10 @@ namespace MusicPlayer_ConsoleApp
             int Offset = 1;
             int RealIndex = Index - Offset;
 
-            Console.WriteLine("Enter Song No to Remove");
+            Console.Write("\n\n" +
+                "Enter Song No to Remove\n" +
+                "Enter 0 to return to Main Menu\n" +
+                "====>  ");
             string Input = Console.ReadLine();
             if (reg1.IsMatch(Input))
             {
@@ -188,14 +190,22 @@ namespace MusicPlayer_ConsoleApp
                 int Limit = MyPlaylist[MyListPlaylist[RealIndex]].Count;
                 if (Count <= Index & Index <= Limit)
                 {
-                    MyPlaylist[MyListPlaylist[RealIndex]].Remove(MyPlaylist[MyListPlaylist[RealIndex]][SongIndex]);
-                    goto start;
+                    switch (Index2)
+                    {
+                        case 0:
+                            Console.Clear();
+                            Program.Run();
+                            break;
+                        default:
+                            MyPlaylist[MyListPlaylist[RealIndex]].Remove(MyPlaylist[MyListPlaylist[RealIndex]][SongIndex]);
+                            goto start;
+                    }
                 }
                 else
                 {
                     Console.Clear();
                     Console.WriteLine($"{Input} is an invalid Option");
-                    //DisplayPlaylist();
+        
                     goto start;
 
 
@@ -209,7 +219,8 @@ namespace MusicPlayer_ConsoleApp
             int Offset = 1;
             int RealIndex = Index - Offset;
 
-            Console.Write($"Enter Song to Add to playlist \"{MyListPlaylist[RealIndex]}\"\n" +
+            Console.Write("\n\n" +
+                $"Enter Song to Add to playlist \"{MyListPlaylist[RealIndex]}\"\n" +
                 "Enter 0 to return to Main Menu\n" +
                 "====>  ");
             string Input = Console.ReadLine();
@@ -227,13 +238,134 @@ namespace MusicPlayer_ConsoleApp
                         AddingSong(Index);
                         break;
                 }
-                
+
             }
 
         }
         public static void EditSongsInPlaylist()
         {
-            DisplayPlaylist();
+        start: Count = 0;
+            Console.WriteLine("Enter the Numbers to Select Playlist to Delete");
+            foreach (KeyValuePair<string, List<string>> list in MyPlaylist)
+            {
+                Count++;
+                Console.Write($"{Count}:  ");
+                Console.WriteLine(list.Key);
+                MyListPlaylist.Add(list.Key);
+
+            }
+            Console.WriteLine("Enter 0 To Return to Main Menu");
+            string? Option = Console.ReadLine();
+            if (reg1.IsMatch(Option))
+            {
+                Count = 0;
+
+                int Index = Convert.ToInt32(Option);
+                int Limit = MyPlaylist.Count;
+                if (Count <= Index & Index <= Limit)
+                {
+                    switch (Index)
+                    {
+                        case 0:
+                            Console.Clear();
+                            Program.Run();
+                            break;
+                        default:
+
+                            EditingSong(Index);
+                            break;
+
+
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{Index} is an invalid Option");
+
+                    goto start;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"{Option} is an invalid Option");
+
+                goto start;
+            }
+
+        }
+        private static void EditingSong(int Index)
+        {
+        start: ShowSongsinPlaylist(Index);
+            int Offset = 1;
+            int RealIndex = Index - Offset;
+
+            Console.Write("\n\n" +
+                "Enter Song No to Edit\n" +
+                "Enter 0 to return to Main Menu\n" +
+                "====>  ");
+            string Input = Console.ReadLine();
+            if (reg1.IsMatch(Input))
+            {
+                int Index2 = Convert.ToInt32(Input);
+                int SongIndex = Index2 - 1;
+
+               
+                Count = 0;
+
+                
+                int Limit = MyPlaylist[MyListPlaylist[RealIndex]].Count;
+               
+               
+                if (Count <= Index & Index <= Limit)
+                {
+                    switch (Index2)
+                    {
+                        case 0:
+                            Console.Clear();
+                            Program.Run();
+                            break;
+                        default:
+                            Console.Clear();
+                      here: Console.Write($"Enter Song to Replace {MyPlaylist[MyListPlaylist[RealIndex]][SongIndex]} with\n" +
+                            "====>  ");
+                            string Input2 = Console.ReadLine();
+                            if (reg.IsMatch(Input2))
+                            {
+                                MyPlaylist[MyListPlaylist[RealIndex]][SongIndex] = Input2;
+                                EditingSong(Index);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{Input2} is not a valid input");
+                                goto here;
+                            }
+                            
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{Input} is an invalid Option");
+            
+                    goto start;
+
+
+                }
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"{Input} is an invalid Option");
+       
+                goto start;
+
+
+            }
+
         }
 
     }
